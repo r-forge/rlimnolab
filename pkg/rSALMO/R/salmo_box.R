7bc0f322f45e307af81a3444083b0ba975fd5706
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2013-04-26 17:06:53 +0000

    ... continued
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rlimnolab@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rSALMO/R/salmo_box.R
+++ pkg/rSALMO/R/salmo_box.R
@@ -28,3 +28,28 @@ SALMO.1box <- function(time, x, p, inputs) {
   #list(dx, dO = unname(dx[8]))
   list(dx, iin = unname(uu["iin"]))
 }
+
+
+SALMO.2box <- function(time, x, p, inputs) {
+  #cat(time, "\n")
+
+  ## interpolate data (this is the slowest part of the simulation)
+  uu <- approxTime1(inputs, time)
+
+  ## call the C model core for the upper layer (epilinion)
+  ret <- call_salmodll("SalmoCore", p$nOfVar, p$cc, p$pp, uu, x)
+
+  ## modify uu so that it describes the bottom layer (hypolimnion)
+
+  ## call the C model core for the bottom layer
+
+  ## apply partial mixing of the layers
+
+  ## return source - sink terms
+  dx <-c(ret[[2]] - ret[[3]])
+
+  ## fix oxygen balance at surface to saturated value
+  dx[8] <-  o2sat(uu["temp"]) - x[8]
+  #list(dx, dO = unname(dx[8]))
+  list(dx, iin = unname(uu["iin"]))
+}
