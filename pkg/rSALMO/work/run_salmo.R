915e855f4f7d2ce358942e857baf93e7242048be
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2013-05-22 13:04:02 +0000

    aver ...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rlimnolab@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rSALMO/work/run_salmo.R
+++ pkg/rSALMO/work/run_salmo.R
@@ -43,11 +43,11 @@ data(pp)  # phytoplankton parameters, matrix with 1 column per phytopl. species
 forc <- with(bautzen1997, 
   data.frame(
     time   = t,
-    vol    = v,
+    vol    = ve, # v
     depth  = s - 154,
-    dz     = 5, #zmix,
+    dz     = 5,  # zmix,
     qin    = qin,
-    ased   = 0,
+    ased   = ve / 5, # ve / zmix OR v/zmix
     srf    = 0,     # <-----
     iin    = iin,
     temp   = temp,
@@ -56,14 +56,13 @@ forc <- with(bautzen1997,
     pomin  = pomin,
     zin    = zin,
     oin    = o2sat(temp),
-    aver   = 0,
+    aver   = 0,      # open water part of bottom area (1 - a_sed/a_layer) 
     ad     = 0,
     au     = 0,
     diff   = 0,
     x1in   = xin1,
     x2in   = xin2,
-    x3in   = 0,
-    sf     = 1      # <-----
+    x3in   = 0
   )
 )
 forc <- as.matrix(forc)
@@ -72,13 +71,13 @@ forc <- as.matrix(forc)
 # NOFVAR
 nOfVar <- c(
   numberOfInputs      = 21,
-  numberOfOutputs     = 14,
+  numberOfOutputs     = 12,
   numberOfStates      = 11,
   numberOfParameters  = NA,
   numberOfAlgae       = 3,
   numberOfLayers      = 1,
-  numberOfTributaries = 4,
-  numberOfOutlets     = 3,
+  numberOfTributaries = 1,
+  numberOfOutlets     = 1,
   timestep = 1
 ) 
 
@@ -95,7 +94,7 @@ x0 <- c(N=5, P=10, X1=.1, X2=.1,  X3=.1, Z=.1, D=20, O=14, G1=0, G2=0, G3=0)
 
 
 ## call one time for testing
-ret <- call_SalmoDLL("SalmoCore", nOfVar, cc, pp, forc, x0)
+#ret <- call_SalmoDLL("SalmoCore", nOfVar, cc, pp, forc, x0)
 
 
 times <- seq(0, 365, 1)
@@ -105,6 +104,13 @@ times <- seq(0, 365, 1)
 #)
 
 plot(out, which=c("N", "P", "X1", "X2", "X3", "Z", "D", "O", "iin"))
+
+forc2 <- forc
+forc2[, "pin"] <- 0.1 * forc[, "pin"]
+
+out2 <- ode(x0, times, SALMO.1box, parms = NULL, method="lsoda", inputs=forc2)
+
+plot(out, out2, which=c("N", "P", "X1", "X2", "X3", "Z", "D", "O", "iin"))
 
 
 ## Todo: 
