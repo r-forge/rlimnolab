d75e25e56ca522ec510503ef6cd8de224d02b57f
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2013-06-14 07:26:40 +0000

    tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rlimnolab@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rSALMO/work/test_approxTime.R
+++ pkg/rSALMO/work/test_approxTime.R
@@ -3,10 +3,39 @@ x <- data.frame(time = c(1,2,4,5,7,11),
               y2 = runif(6)
 )
 
+x$time <- x$time * 0.3
 
-matplot(x[,1], x[,-1], type="l", xlim=c(-2,15))
+matplot(x[,1], x[,-1], type="l")
 
-approxTime1(x, 14)
+#ret <- approxTime1(x, 7.5)
+#matpoints(ret[1], t(ret[-1]), pch=16)
 
-x
-approxTime1(x, 2.5, rule=2)
+
+(ret <- approxTimeEq(x, 1.6))
+matpoints(ret[1], ret[-1], pch=16)
+
+
+size <- 1e2
+x <-  x <- data.frame(time = (1:size) * 1.5 - 50,
+              y1 = runif(size),
+              y2 = runif(size)
+)
+
+matplot(x[,1], x[,-1], type="l")
+for (i in 1:10) {
+  ret <- approxTimeEq(x, xo <- runif(3, min=min(x$time), max=max(x$time)))
+  print(ret)
+  matpoints(ret[,1], ret[,-1], pch=16)
+}
+
+system.time(
+  for (i in 1:10)
+    ret <- approxTimeEq(x, xo <- runif(1, min=min(x$time), max=max(x$time)))
+)
+
+system.time(
+  for (i in 1:10)
+    ret <- approxTime(x, -1)
+)
+
+
