a501daf5cbfaa96eb5557e84aec74d8a6d13f664
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2013-09-05 06:46:30 +0000

    make_parvec.R simplified. Needs testing!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rlimnolab@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rSALMO/work/make_parvec.R
+++ pkg/rSALMO/work/make_parvec.R
@@ -2,7 +2,14 @@
 
 ccx <- read.table("cc.txt")
 
+## convert data frame to named vector
+
+## original method
 cc <- as.vector(ccx[,2])
 names(cc) <- ccx[,1]
+
+## simplified, but not yet tested
+#cc <- structure(as.vector(ccx[,2]), names = as.character(ccx[,1]))
+
 save(cc, file="cc.rda")
-     
+
